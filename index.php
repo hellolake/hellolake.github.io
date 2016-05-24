27d256194aa8eb0040bff3b8c165ad9d42632e2d
Lake <hellolake@outlook.com>
2016-05-24 16:50:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+	<title>Lake - 世界诞生于混沌，生命起源于热海。</title>
+	<link rel="stylesheet" href="css/index.css?v5">
+    <!--[if lt IE 9]><link type="text/css" rel="stylesheet" href="css/index_lt_ie9.css" /><![endif]-->
+	<script src="lake.js/js/lake.min.js"></script>
+	<script src="js/index.js?v5"></script>
+</head>
+<body>
+	<header>
+        <div class="video">
+            <div class="video_wrap">
+                <video preload="metadata">
+                    <source src="video/A_Chinese_Selection.webm" type='video/webm; codecs="vp8.0, vorbis"'>
+                    <source src="video/A_Chinese_Selection.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+<!--                    <source src="video/A_Chinese_Selection.ogv" type='video/ogg; codecs="theora, vorbis"'>-->
+                </video>
+                <img class="one" src="img/A_Chinese_Selection.jpg">
+            </div>
+            <div class="top">
+                <p><a class="mail" href="mailto:hellolake@sina.com">hellolake@sina.com</a><span></span><a class="weibo" href="http://weibo.com/hellolake"></a><a class="github" href="https://github.com/hellolake/lake.js"></a></p>
+            </div>
+            <div class="video_ctrl">
+                <div class="lake"><a class="radiate" title="Me" href="http://weibo.com/hellolake" target="_blank"><img src="img/avatar.png"></a></div>
+                <div class="control">
+                    <span class="play" title="Play/Pause">
+<!--                        <i class="left radiate"><img src="img/control_side.png"></i>-->
+<!--                        <i class="right radiate"><img src="img/control_side.png"></i>-->
+                        <a class="left radiate" title="Github" href="https://github.com/hellolake/lake.js" target="_blank"><img src="img/play_github_normal.png"></a>
+                        <a class="right radiate" title="Weibo" href="http://weibo.com/hellolake/" target="_blank"><img src="img/play_weibo_normal.png"></a>
+                        <em class="btn radiate"><img src="img/play_round_normal.png"></em>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </header>
+    <nav><div class="nav_contain"><div class="nav_wrap"><em class="on">JS函数湖</em><span>|</span><em>IxD原型</em><span>|</span><em>UI设计</em><span>|</span><em>Web3D实验</em><span>|</span><em>MV短片</em><span>|</span><em>OTS其他</em></div></div></nav>
+    <section>
+        <div class="content">
+            <div class="content_wrap">
+                <div class="js">
+                    <p class="block_wrap">
+                        <span class="block">
+                            <span class="img">
+                                <a href="/lake.bmap.js">
+                                    <img src="img/js/lake.bmap.js.jpg">
+                                </a>
+                            </span>
+                            <span class="desc">
+                                <strong>lake.bmap.js</strong>
+                                <span>更强大的自定义覆盖物</span>
+                                <span>更灵活的自定义路径/途经点</span>
+                                <span>方便的定位控制</span>
+                                <span>解决多地图图块加载问题</span>
+                                <span>...</span>
+                                <span>"BTW,英文注释纯属练习"</span>
+                                <span>"促进人类进步,不做低级兼容. F.H.A."</span>
+                            </span>
+                            <span class="btm"><a  class="github" href="https://github.com/hellolake/lake.js">GITHUB</a><a class="demo" href="/lake.bmap.js">DEMO/DOC</a></span>
+                        </span>
+                        <span class="block">
+                            <span class="img">
+                                <a href="https://github.com/hellolake/lake.js">
+                                    <img src="img/js/lake.js.jpg">
+                                </a>
+                            </span>
+                            <span class="desc">
+                                <strong>lake.js</strong>
+                                <span>Simple Function Lake / 简易方法集</span>
+                                <span>持续改进中...</span>
+                            </span>
+                            <span class="btm one"><a class="github" href="https://github.com/hellolake/lake.js">GITHUB</a></span>
+                        </span>
+                        <span class="block">
+                            <span class="img">
+                                <a href="javascript:;">
+                                    <img src="img/tobecontinue.jpg">
+                                </a>
+                            </span>
+                            <span class="desc">
+                                <strong>TO BE CONTINUE</strong>
+                                <span>待续...</span>
+                            </span>
+                            <span class="btm one"><a class="lotus" href="javascript:;"></a></span>
+                        </span>
+                    </p>
+                </div>
+                <div class="ixd">
+                    <p class="block_wrap">
+                        <span class="block">
+                            <span class="img">
+                                <a href="javascript:;">
+                                    <img src="img/tobecontinue.jpg">
+                                </a>
+                            </span>
+                            <span class="desc">
+                                <strong>TO BE CONTINUE</strong>
+                                <span>待续...</span>
+                            </span>
+                            <span class="btm one"><a class="lotus" href="javascript:;"></a></span>
+                        </span>
+                    </p>
+                </div>
+                <div class="ui">
+                    <p class="block_wrap">
+                        <span class="block">
+                            <span class="img">
+                                <a href="javascript:;">
+                                    <img src="img/tobecontinue.jpg">
+                                </a>
+                            </span>
+                            <span class="desc">
+                                <strong>TO BE CONTINUE</strong>
+                                <span>待续...</span>
+                            </span>
+                            <span class="btm one"><a class="lotus" href="javascript:;"></a></span>
+                        </span>
+                    </p>
+                </div>
+                <div class="web3d">
+                    <p class="block_wrap">
+                        <span class="block">
+                            <span class="img">
+                                <a href="javascript:;">
+                                    <img src="img/tobecontinue.jpg">
+                                </a>
+                            </span>
+                            <span class="desc">
+                                <strong>TO BE CONTINUE</strong>
+                                <span>待续...</span>
+                            </span>
+                            <span class="btm one"><a class="lotus" href="javascript:;"></a></span>
+                        </span>
+                    </p>
+                </div>
+                <div class="mv">
+                    <p class="block_wrap">
+                        <span class="block">
+                            <span class="img">
+                                <a href="javascript:;">
+                                    <img src="img/tobecontinue.jpg">
+                                </a>
+                            </span>
+                            <span class="desc">
+                                <strong>TO BE CONTINUE</strong>
+                                <span>待续...</span>
+                            </span>
+                            <span class="btm one"><a class="lotus" href="javascript:;"></a></span>
+                        </span>
+                    </p>
+                </div>
+                <div class="ots">
+                    <p class="block_wrap">
+                        <span class="block">
+                            <span class="img">
+                                <a href="javascript:;">
+                                    <img src="img/tobecontinue.jpg">
+                                </a>
+                            </span>
+                            <span class="desc">
+                                <strong>TO BE CONTINUE</strong>
+                                <span>待续...</span>
+                            </span>
+                            <span class="btm one"><a class="lotus" href="javascript:;"></a></span>
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer></footer>
+    <div id="log"></div>
+</body>
+<script>
+
+    if((document.selection && !document.msElementsFromPoint) || !document.addEventListener){
+        var warning = document.createElement("p");
+        warning.style.margin = "1em";
+        warning.innerHTML = "请使用现代浏览器或IE9以上打开本站，下载：<a href='https://www.baidu.com/s?ie=utf-8&wd=%E8%B0%B7%E6%AD%8C%E6%B5%8F%E8%A7%88%E5%99%A8' target='_blank'>谷歌浏览器</a> | <a href='https://www.baidu.com/s?ie=utf-8&wd=%E7%81%AB%E7%8B%90%E6%B5%8F%E8%A7%88%E5%99%A8' target='_blank'>火狐浏览器</a>";
+        document.body.insertBefore(warning,document.body.childNodes[0]);
+    }
+
+    console.log(lake.select(".content"));
+
+    //        document.oncontextmenu = function(){return false};
+
+</script>
+</html>
+
